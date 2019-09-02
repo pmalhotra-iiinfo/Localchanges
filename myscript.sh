715a538bcbd37a1a3e3f4ecd3b0bf227ac2b1c3f
wso2salient <54189506+wso2salient@users.noreply.github.com>
2019-09-02 17:25:28 -0400

    Add files via upload
    script

---
+++ myscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cp ./Localchanges-master/custom.inbound.authenticator-1.0.0 C:/Program Files/WSO2/Identity Server/5.8.0/repository/components/dropins/
+cp ./Localchanges-master/org.wso2.carbon.identity.application.ttbfonl.authenticator.basicauth-1.0.0 C:/Program Files/WSO2/Identity Server/5.8.0/repository/components/dropins/
+cp ./Localchanges-master/org.wso2.carbon.identity.application.ttbponl.authenticator.basicauth-1.0.0 C:/Program Files/WSO2/Identity Server/5.8.0/repository/components/dropins/
+cp ./Localchanges-master/application-authentication.xml C:/Program Files/WSO2/Identity Server/5.8.0/repository/conf/identity/application-authentication.xml
+cp ./Localchanges-master/ttb_login.jsp C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/webapps/authenticationendpoint/ttb_login.jsp
+cp ./Localchanges-master/default_login.jsp C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/webapps/authenticationendpoint/default_login.jsp
+cp ./Localchanges-master/travelocity_login.jsp C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/webapps/authenticationendpoint/travelocity_login.jsp
+cp ./Localchanges-master/avis_app_login.jsp C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/webapps/authenticationendpoint/avis_app_login_login.jsp
+cp ./Localchanges-master/login.jsp C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/webapps/authenticationendpoint/login.jsp
+cp ./Localchanges-master/index.jag C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/jaggeryapps/dashboard/index.jag
+cp ./Localchanges-master/Dollar_sign.png C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/jaggeryapps/dashboard/img/
+cp ./Localchanges-master/Fonl_image.png C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/jaggeryapps/dashboard/img/
+cp ./Localchanges-master/Cola_img.png C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/jaggeryapps/dashboard/img/
+cp ./Localchanges-master/travelocity.com.war C:/Users/hemalkumar.patel/Downloads/Apache_Tomcat_0708/webapps
+cp ./Localchanges-master/avis.com.war C:/Users/hemalkumar.patel/Downloads/Apache_Tomcat_0708/webapps
+ 
