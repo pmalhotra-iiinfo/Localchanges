f2a397fb160fc416a601910601606724fce598c9
wso2salient <54189506+wso2salient@users.noreply.github.com>
2019-09-04 13:32:55 -0400

    Add files via upload
    new script

--- myscript.sh
+++ myscript.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-cp ./Localchanges-master/custom.inbound.authenticator-1.0.0.txt C:/Program Files/WSO2/Identity Server/5.8.0/repository/components/dropins/
-cp ./Localchanges-master/org.wso2.carbon.identity.application.ttbfonl.authenticator.basicauth-1.0.0.txt C:/Program Files/WSO2/Identity Server/5.8.0/repository/components/dropins/
-cp ./Localchanges-master/org.wso2.carbon.identity.application.ttbponl.authenticator.basicauth-1.0.0.txt C:/Program Files/WSO2/Identity Server/5.8.0/repository/components/dropins/
-cp ./Localchanges-master/application-authentication.xml C:/Program Files/WSO2/Identity Server/5.8.0/repository/conf/identity/application-authentication.xml
-cp ./Localchanges-master/basicauth.jsp C:/Program Files/WSO2/Identity Server/5.8.0/repository\deployment\server\webapps\authenticationendpoint\basicauth.jsp
-cp ./Localchanges-master/ttb_login.jsp C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/webapps/authenticationendpoint/
-cp ./Localchanges-master/default_login.jsp C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/webapps/authenticationendpoint/
-cp ./Localchanges-master/travelocity_login.jsp C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/webapps/authenticationendpoint/
-cp ./Localchanges-master/avis_app_login.jsp C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/webapps/authenticationendpoint/
-cp ./Localchanges-master/login.jsp C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/webapps/authenticationendpoint/login.jsp
-cp ./Localchanges-master/index.jag C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/jaggeryapps/dashboard/index.jag
-cp ./Localchanges-master/Dollar_sign.png C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/jaggeryapps/dashboard/img/
-cp ./Localchanges-master/Fonl_image.png C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/jaggeryapps/dashboard/img/
-cp ./Localchanges-master/Cola_img.png C:/Program Files/WSO2/Identity Server/5.8.0/repository/deployment/server/jaggeryapps/dashboard/img/
-cp ./Localchanges-master/travelocity.com.war.txt C:/Users/hemalkumar.patel/Downloads/Apache_Tomcat_0708/webapps
-cp ./Localchanges-master/avis.com.war.txt C:/Users/hemalkumar.patel/Downloads/Apache_Tomcat_0708/webapps
-cp ./Localchanges-master/index.html C:/Users/hemalkumar.patel/Downloads/Apache_Tomcat_0708/webapps/myApp/
-cp -r ./Localchanges-master/img C:/Users/hemalkumar.patel/Downloads/Apache_Tomcat_0708/webapps/img
+cp ./Localchanges-master/custom.inbound.authenticator-1.0.0.jar /C/Program\ Files/WSO2/Identity\ Server/5.8.0/repository/components/dropins/
+cp ./Localchanges-master/org.wso2.carbon.identity.application.ttbfonl.authenticator.basicauth-1.0.0.jar /C/Program\ Files/WSO2/Identity\ Server/5.8.0/repository/components/dropins
+cp ./Localchanges-master/org.wso2.carbon.identity.application.ttbponl.authenticator.basicauth-1.0.0.jar /C/Program\ Files/WSO2/Identity\ Server/5.8.0/repository/components/dropins
+cp ./Localchanges-master/application-authentication.xml /C/Program\ Files/WSO2/Identity\ Server/5.8.0/repository/conf/identity/application-authentication.xml
+cp ./Localchanges-master/basicauth.jsp /C/Program\ Files/WSO2/Identity\ Server/5.8.0/repository/deployment/server/webapps/authenticationendpoint/basicauth.jsp
+cp ./Localchanges-master/ttb_login.jsp /C/Program\ Files/WSO2/Identity\ Server/5.8.0/repository/deployment/server/webapps/authenticationendpoint/
+cp ./Localchanges-master/default_login.jsp /C/Program\ Files/WSO2/Identity\ Server/5.8.0/repository/deployment/server/webapps/authenticationendpoint/
+cp ./Localchanges-master/travelocity_login.jsp /C/Program\ Files/WSO2/Identity\ Server/5.8.0/repository/deployment/server/webapps/authenticationendpoint/
+cp ./Localchanges-master/avis_app_login.jsp /C/Program\ Files/WSO2/Identity\ Server/5.8.0/repository/deployment/server/webapps/authenticationendpoint/
+cp ./Localchanges-master/login.jsp /C/Program\ Files/WSO2/Identity\ Server/5.8.0/repository/deployment/server/webapps/authenticationendpoint/login.jsp
+cp ./Localchanges-master/index.jag /C/Program\ Files/WSO2/Identity\ Server/5.8.0/repository/deployment/server/jaggeryapps/dashboard/index.jag
+cp ./Localchanges-master/Dollar_sign.png /C/Program\ Files/WSO2/Identity\ Server/5.8.0/repository/deployment/server/jaggeryapps/dashboard/img/
+cp ./Localchanges-master/Fonl_image.png /C/Program\ Files/WSO2/Identity\ Server/5.8.0/repository/deployment/server/jaggeryapps/dashboard/img/
+cp ./Localchanges-master/Cola_img.png /C/Program\ Files/WSO2/Identity\ Server/5.8.0/repository/deployment/server/jaggeryapps/dashboard/img/
+cp ./Localchanges-master/travelocity.com.war /C/Users/hemal/Downloads/Apache_Tomcat_0902/apache-tomcat-9.0.24/webapps
+cp ./Localchanges-master/avis.com.war /C/Users/hemal/Downloads/Apache_Tomcat_0902/apache-tomcat-9.0.24/webapps
+cp -r ./Localchanges-master/img /C/Users/hemal/Downloads/Apache_Tomcat_0902/apache-tomcat-9.0.24/webapps/myApp
+cp ./Localchanges-master/index.html /C/Users/hemal/Downloads/Apache_Tomcat_0902/apache-tomcat-9.0.24/webapps/myApp/
+
  
